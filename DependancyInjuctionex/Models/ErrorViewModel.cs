2eaea8a09e3ed08ea4540bf237120e1fa0c626d7
Your Sudharshan <sudhareshanmech@gmail.com>
2023-08-29 16:05:27 +0530

    My DI

---
+++ DependancyInjuctionex/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DependancyInjuctionex.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
